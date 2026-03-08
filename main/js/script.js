4fa0872f8cac7b75621d660fd8dd5c42c42e0438
mayu0066 <mayu.6.emih@outlook.jp>
2026-03-08 22:52:28 +0900

    htmlを１つに統一して画像や文章を更新

--- main/js/script.js
+++ main/js/script.js
@@ -18,3 +18,44 @@ $(function () {
   });
 
 });
+
+// オプション設定
+// 条件：要素の30% 画面に入ったら発火
+const itemOption = {
+  root: null,
+  rootMargin: "0px",
+  threshold: 0.2,
+}
+
+const titleOption = {
+  root: null,
+  rootMargin: "0px",
+  threshold: 0.1,
+}
+
+// インスタンス生成　コールバック関数とオプションを渡す
+const observerItem = new IntersectionObserver(doWhenIntersect, itemOption);
+const observerTitle = new IntersectionObserver(doWhenIntersect, titleOption);
+
+// 監視対象にしたい要素を渡す
+document.querySelectorAll('.js--fade-item').forEach((el) => {
+  observerItem.observe(el);
+});
+
+document.querySelectorAll('.js--fade-title').forEach((el) => {
+  observerTitle.observe(el);
+});
+
+function doWhenIntersect(entries) {
+  entries.forEach(entry => {
+
+    if (entry.isIntersecting) {
+      // 画面に入ったらクラスを付ける
+      entry.target.classList.add('is-intersecting');
+    } else {
+      // 画面から出たらクラスを外す
+      entry.target.classList.remove('is-intersecting');
+    }
+
+  });
+}
